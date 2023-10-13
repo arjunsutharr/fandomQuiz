aaa1abda7d5d343f62a75e3238a0b8cb01d5454a
Arjun Suthar <arjunsuthariit@gmail.com>
2023-10-13 17:19:28 +0530

    docs: updated files

---
+++ README.md
@@ -0,0 +1,10 @@
+This is a simple quiz game written in JavaScript. The game asks the user a series of questions and keeps track of the user's score. At the end of the game, the user's score is displayed along with a list of the high scores.
+
+To play the game, simply run the code and answer the questions as best you can. The game will let you know if you answered correctly or not. At the end of the game, your score will be displayed along with a list of the high scores.
+
+The game uses the following libraries:
+
+Chalk: For coloring the output.
+readline-sync: For getting input from the user.
+
+You can modify the game to add more questions, change the difficulty, or change the way the scores are kept track of.
---
+++ index.js
@@ -0,0 +1,118 @@
+var chalk = require("chalk");
+var readlineSync = require("readline-sync");
+var userName = readlineSync.question("who is there? ");
+console.log(chalk.hex('#78D6C6')("Welcome to the" + userName + "game! Answer the questions correctly to get a high score. "));
+console.log(chalk.hex('#1F4172')(" \nLet's play a game."));
+
+var score = 0;
+
+
+function gamePlay(question, answer){
+  var userAnswer = readlineSync.question(question);
+  if(userAnswer.toUpperCase() ===  answer.toUpperCase()){
+    console.log(chalk.grey("You answer: " + answer + "."));
+    console.log(chalk.green("You are right. "));
+    score++;
+  } else{
+    console.log(chalk.grey("You answer: " + answer + "."));
+    console.log(chalk.red("You are wrong. "));
+  }
+
+  console.log(chalk.hex('#EE9322')("Your current score is: " + score + "\n"));
+}
+
+var questions = [
+  {
+    question: 
+    chalk.grey(` 
+    1. What is solution of all problems of your life? 
+    a: Inner Engineering by Sadhguru
+    b: Alcohol
+    c: Drugs
+    `),    
+    answer:"a"
+    },
+    {
+    question: chalk.grey( 
+    `2. In a website browser address bar, what does “www” stand for? 
+     a: World wide wab
+     b: World wide web
+     c: World web wide
+     d: web world wide
+     `),
+    answer: "b"
+    },
+    {
+    question: chalk.grey(
+    `3. In what year was the internet opened to the public? 
+     a: 1999
+     b: 1947
+     c: 1985
+     d: 1993
+     `),
+    answer: "d"
+    },
+    {
+    question: chalk.grey( 
+    `4. Where was the world’s first university established in India?
+     a: Takshashila
+     b: Nalanda
+     c: kolkata
+     d: Delhi
+     `),
+    answer:"a"
+    },
+    {
+    question: chalk.grey(
+    `5. What is the national animal of India?
+     a: Peacock,
+     b: Peahens
+     c: Bengal Tiger
+     d: Parrot
+     `),
+    answer: "c"
+    }
+]
+
+
+
+for(var i = 0; i < questions.length; i++){
+  var ques = questions[i];
+  gamePlay(ques.question , ques.answer);
+}
+
+
+console.log("your total score is: " + score + "/5 \n");
+
+var highScoreList = [
+  {
+  name: "Arpit ",
+  score: 5 
+  },
+  { 
+  name: "Madhav ",
+  score: 3 
+  },
+  { 
+  name: "Pritam ",
+  score: 4 
+  },
+  {
+    name: "You",
+    score: score
+  }
+]
+
+function sortHighScore(highScores){
+    return highScores.sort((a, b) => {
+        return b.sort - a.sort;
+    });
+}
+
+var sortedHighScoreList = sortHighScore(highScoreList);
+
+for(var highScores of sortedHighScoreList){
+    console.log(highScores.name + ": " + highScores.score);
+}
+
+console.log("\n Thank you for playing.")
